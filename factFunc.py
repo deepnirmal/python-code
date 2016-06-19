8fa76bdce785e28956d2ab34a8c64363cc511761
Deep S Nirmal <deep2432@gmail.com>
2016-06-19 16:58:31 +0530

    Function for factorial

---
+++ factFunc.py
@@ -0,0 +1,13 @@
+
+
+
+def fact(number) :
+	product=1 
+	for i in range(1,number+1):
+		product*=i
+	return product
+
+print("Enter a number : ")
+num=int(input())
+
+print("Factorial of "+str(num)+" is : "+str(fact(num)))
