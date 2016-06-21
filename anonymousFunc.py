75ea87a65cb4d4349b07b558ecd5c7c1a06dc5b0
Deep S Nirmal <deep2432@gmail.com>
2016-06-21 23:48:12 +0530

    Anonymous function Exmaple with map and list

---
+++ anonymousFunc.py
@@ -0,0 +1,16 @@
+#Anonymous function
+#known as Lambda form
+
+
+#def square(number) :
+# 	return number*number
+
+
+square= lambda x: x*x
+print(square(2))
+
+
+numbers=[1,2,3,4]
+
+numbersqr=list(map(lambda x:x*x,numbers))
+print(numbersqr)
