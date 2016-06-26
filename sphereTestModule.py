cc1721ef791a4a434e68bbf84ea136ad96ecf469
Deep S Nirmal <deep2432@gmail.com>
2016-06-26 17:28:32 +0530

    calculting area of volume of sphere using namespace or module

---
+++ sphereTestModule.py
@@ -0,0 +1,10 @@
+from moduleSphere import * 
+import moduleSphere
+print("Enter the radius of the radius: ")
+radius=int(input())
+
+#print("The area is : "+str(area(radius)))
+#print("The volume is : "+str(volume(radius)))
+
+print("The area is : "+str(moduleSphere.area(radius)))
+print("The volume is : "+str(moduleSphere.volume(radius)))
